7a0b4746ffdb57bc190e90254b018cc587f2d02d
Nade <145964140+NadeZz11@users.noreply.github.com>
2025-06-26 16:45:26 +0700

    while-loop commit

---
+++ while-loop/ASCIIConverter.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+    while(1){
+        int x;
+        char a;
+        scanf("%d",&x);
+        if(x==-1){
+            break;
+        }
+        printf("%c\n",x);
+    }
+
+}
---
+++ while-loop/CountingDown.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    int N;
+    scanf("%d",&N);
+    int i=N;
+    while(i>=1){
+        printf("%d\n",i);
+        i--;
+    }
+}
